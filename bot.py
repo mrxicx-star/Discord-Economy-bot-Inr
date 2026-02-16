ca8ff699c392faf01d916c4e5f8e0506400dbd42
mrxicx-star <mrxicx@gmail.com>
2026-02-16 17:15:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 bot.remove_command("help")  # Disable default help
 
+
 # ----------------------------
 # DATABASE SYSTEM
 # ----------------------------
